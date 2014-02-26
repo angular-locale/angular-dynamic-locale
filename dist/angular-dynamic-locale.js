20fb28a74fe907be895f392ee5c968a26d3e0c06
Carey Hinoki <carey@shift.com>
2014-02-26 14:06:15 -0800

    chore(build): Build v0.1.2

--- dist/angular-dynamic-locale.js
+++ dist/angular-dynamic-locale.js
@@ -1,8 +1,8 @@
 /**
- * Angular-Dynamic-Locale v0.1.0
+ * Angular-Dynamic-Locale v0.1.2
  *
  * @author: Carey Hinoki <carey.hinoki@gmail.com> (http://careyhinoki.me/)
- * @date: 2014-02-21
+ * @date: 2014-02-26
  */
 
 
@@ -16,42 +16,7 @@
 (function() {
   var app, dl;
 
-  dl = angular.module('dynamic.locale', ['ng']);
-
-  app = angular.module('app', ['dynamic.locale']);
-
-  app.config([
-    'dlProviderProvider', function(dlProviderProvider) {
-      dlProviderProvider.setDefaultLocale('en-us');
-      dlProviderProvider.setLocales(['af-na', 'af-za', 'af', 'am-et', 'am', 'ar-001', 'ar-ae', 'ar-bh', 'ar-dz', 'ar-eg', 'ar-iq', 'ar-jo', 'ar-kw', 'ar-lb', 'ar-ly', 'ar-ma', 'ar-om', 'ar-qa', 'ar-sa', 'ar-sd', 'ar-sy', 'ar-tn', 'ar-ye', 'ar', 'bg-bg', 'bg', 'bn-bd', 'bn-in', 'bn', 'ca-ad', 'ca-es', 'ca', 'cs-cz', 'cs', 'da-dk', 'da', 'de-at', 'de-be', 'de-ch', 'de-de', 'de-li', 'de-lu', 'de', 'el-cy', 'el-gr', 'el', 'en-as', 'en-au', 'en-bb', 'en-be', 'en-bm', 'en-bw', 'en-bz', 'en-ca', 'en-dsrt-us', 'en-dsrt', 'en-fm', 'en-gb', 'en-gu', 'en-gy', 'en-hk', 'en-ie', 'en-in', 'en-iso', 'en-jm', 'en-mh', 'en-mp', 'en-mt', 'en-mu', 'en-na', 'en-nz', 'en-ph', 'en-pk', 'en-pr', 'en-pw', 'en-sg', 'en-tc', 'en-tt', 'en-um', 'en-us', 'en-vg', 'en-vi', 'en-za', 'en-zw', 'en', 'es-419', 'es-ar', 'es-bo', 'es-cl', 'es-co', 'es-cr', 'es-do', 'es-ea', 'es-ec', 'es-es', 'es-gq', 'es-gt', 'es-hn', 'es-ic', 'es-mx', 'es-ni', 'es-pa', 'es-pe', 'es-pr', 'es-py', 'es-sv', 'es-us', 'es-uy', 'es-ve', 'es', 'et-ee', 'et', 'eu-es', 'eu', 'fa-af', 'fa-ir', 'fa', 'fi-fi', 'fi', 'fil-ph', 'fil', 'fr-be', 'fr-bf', 'fr-bi', 'fr-bj', 'fr-bl', 'fr-ca', 'fr-cd', 'fr-cf', 'fr-cg', 'fr-ch', 'fr-ci', 'fr-cm', 'fr-dj', 'fr-fr', 'fr-ga', 'fr-gf', 'fr-gn', 'fr-gp', 'fr-gq', 'fr-km', 'fr-lu', 'fr-mc', 'fr-mf', 'fr-mg', 'fr-ml', 'fr-mq', 'fr-ne', 'fr-re', 'fr-yt', 'fr', 'gl-es', 'gl', 'gsw-ch', 'gsw', 'gu-in', 'gu', 'he-il', 'he', 'hi-in', 'hi', 'hr-hr', 'hr', 'hu-hu', 'hu', 'id-id', 'id', 'in', 'is-is', 'is', 'it-it', 'it-sm', 'it', 'iw', 'ja-jp', 'ja', 'kn-in', 'kn', 'ko-kr', 'ko', 'ln-cd', 'ln', 'lt-lt', 'lt', 'lv-lv', 'lv', 'ml-in', 'ml', 'mr-in', 'mr', 'ms-my', 'ms', 'mt-mt', 'mt', 'nl-cw', 'nl-nl', 'nl-sx', 'nl', 'no', 'or-in', 'or', 'pl-pl', 'pl', 'pt-br', 'pt-pt', 'pt', 'ro-ro', 'ro', 'ru-ru', 'ru', 'sk-sk', 'sk', 'sl-si', 'sl', 'sq-al', 'sq', 'sr-cyrl-rs', 'sr-latn-rs', 'sr', 'sv-se', 'sv', 'sw-tz', 'sw', 'ta-in', 'ta', 'te-in', 'te', 'th-th', 'th', 'tl', 'tr-tr', 'tr', 'uk-ua', 'uk', 'ur-pk', 'ur', 'vi-vn', 'vi', 'zh-cn', 'zh-hans-cn', 'zh-hk', 'zh-tw', 'zh', 'zu-za', 'zu']);
-      dlProviderProvider.setCurrencyMap({
-        'CAD': 'en-ca',
-        'EUR': 'de',
-        'GBP': 'en-gb',
-        'USD': 'en-us'
-      });
-      return dlProviderProvider.loadLocales();
-    }
-  ]);
-
-  app.controller('AngularDynamicLocaleController', [
-    '$scope', 'dlCurrencySymbol', 'dlProvider', function($scope, dlCurrencySymbol, dlProvider) {
-      var localeMap, x;
-      localeMap = ['af-na', 'af-za', 'af', 'am-et', 'am', 'ar-001', 'ar-ae', 'ar-bh', 'ar-dz', 'ar-eg', 'ar-iq', 'ar-jo', 'ar-kw', 'ar-lb', 'ar-ly', 'ar-ma', 'ar-om', 'ar-qa', 'ar-sa', 'ar-sd', 'ar-sy', 'ar-tn', 'ar-ye', 'ar', 'bg-bg', 'bg', 'bn-bd', 'bn-in', 'bn', 'ca-ad', 'ca-es', 'ca', 'cs-cz', 'cs', 'da-dk', 'da', 'de-at', 'de-be', 'de-ch', 'de-de', 'de-li', 'de-lu', 'de', 'el-cy', 'el-gr', 'el', 'en-as', 'en-au', 'en-bb', 'en-be', 'en-bm', 'en-bw', 'en-bz', 'en-ca', 'en-dsrt-us', 'en-dsrt', 'en-fm', 'en-gb', 'en-gu', 'en-gy', 'en-hk', 'en-ie', 'en-in', 'en-iso', 'en-jm', 'en-mh', 'en-mp', 'en-mt', 'en-mu', 'en-na', 'en-nz', 'en-ph', 'en-pk', 'en-pr', 'en-pw', 'en-sg', 'en-tc', 'en-tt', 'en-um', 'en-us', 'en-vg', 'en-vi', 'en-za', 'en-zw', 'en', 'es-419', 'es-ar', 'es-bo', 'es-cl', 'es-co', 'es-cr', 'es-do', 'es-ea', 'es-ec', 'es-es', 'es-gq', 'es-gt', 'es-hn', 'es-ic', 'es-mx', 'es-ni', 'es-pa', 'es-pe', 'es-pr', 'es-py', 'es-sv', 'es-us', 'es-uy', 'es-ve', 'es', 'et-ee', 'et', 'eu-es', 'eu', 'fa-af', 'fa-ir', 'fa', 'fi-fi', 'fi', 'fil-ph', 'fil', 'fr-be', 'fr-bf', 'fr-bi', 'fr-bj', 'fr-bl', 'fr-ca', 'fr-cd', 'fr-cf', 'fr-cg', 'fr-ch', 'fr-ci', 'fr-cm', 'fr-dj', 'fr-fr', 'fr-ga', 'fr-gf', 'fr-gn', 'fr-gp', 'fr-gq', 'fr-km', 'fr-lu', 'fr-mc', 'fr-mf', 'fr-mg', 'fr-ml', 'fr-mq', 'fr-ne', 'fr-re', 'fr-yt', 'fr', 'gl-es', 'gl', 'gsw-ch', 'gsw', 'gu-in', 'gu', 'he-il', 'he', 'hi-in', 'hi', 'hr-hr', 'hr', 'hu-hu', 'hu', 'id-id', 'id', 'in', 'is-is', 'is', 'it-it', 'it-sm', 'it', 'iw', 'ja-jp', 'ja', 'kn-in', 'kn', 'ko-kr', 'ko', 'ln-cd', 'ln', 'lt-lt', 'lt', 'lv-lv', 'lv', 'ml-in', 'ml', 'mr-in', 'mr', 'ms-my', 'ms', 'mt-mt', 'mt', 'nl-cw', 'nl-nl', 'nl-sx', 'nl', 'no', 'or-in', 'or', 'pl-pl', 'pl', 'pt-br', 'pt-pt', 'pt', 'ro-ro', 'ro', 'ru-ru', 'ru', 'sk-sk', 'sk', 'sl-si', 'sl', 'sq-al', 'sq', 'sr-cyrl-rs', 'sr-latn-rs', 'sr', 'sv-se', 'sv', 'sw-tz', 'sw', 'ta-in', 'ta', 'te-in', 'te', 'th-th', 'th', 'tl', 'tr-tr', 'tr', 'uk-ua', 'uk', 'ur-pk', 'ur', 'vi-vn', 'vi', 'zh-cn', 'zh-hans-cn', 'zh-hk', 'zh-tw', 'zh', 'zu-za', 'zu'];
-      $scope.dlCurrencySymbol = dlCurrencySymbol;
-      return $scope.values = (function() {
-        var _i, _results;
-        _results = [];
-        for (x = _i = 1; _i <= 10000; x = ++_i) {
-          _results.push({
-            currency: parseFloat(Math.random() * (20 - 10) + 10).toFixed(2),
-            locale: localeMap[Math.floor(Math.random() * 245)]
-          });
-        }
-        return _results;
-      })();
-    }
-  ]);
+  dl = angular.module('dl', ['cc']);
 
 
   /*
@@ -79,132 +44,6 @@
       };
     }
   ]);
-
-
-  /*
-  @chalk
-  
-  @name Currency Symbol
-  @description
-  
-  @see http://www.xe.com/symbols.php
-   */
-
-  dl.constant('dlCurrencySymbol', {
-    'ALL': '\u004c\u0065\u006b',
-    'AFN': '\u060b',
-    'ARS': '\u0024',
-    'AWG': '\u0192',
-    'AUD': '\u0024',
-    'AZN': '\u043c\u0430\u043d',
-    'BSD': '\u0024',
-    'BBD': '\u0024',
-    'BYR': '\u0070\u002e',
-    'BZD': '\u0042\u005a\u0024',
-    'BMD': '\u0024',
-    'BOB': '\u0024\u0062',
-    'BAM': '\u004b\u004d',
-    'BWP': '\u0050',
-    'BGN': '\u043b\u0432',
-    'BRL': '\u0052\u0024',
-    'BND': '\u0024',
-    'KHR': '\u17db',
-    'CAD': '\u0024',
-    'KYD': '\u0024',
-    'CLP': '\u0024',
-    'CNY': '\u00a5',
-    'COP': '\u0024',
-    'CRC': '\u20a1',
-    'HRK': '\u006b\u006e',
-    'CUP': '\u20b1',
-    'CZK': '\u004b\u010d',
-    'DKK': '\u006b\u0072',
-    'DOP': '\u0052\u0044\u0024',
-    'XCD': '\u0024',
-    'EGP': '\u00a3',
-    'SVC': '\u0024',
-    'EEK': '\u006b\u0072',
-    'EUR': '\u20ac',
-    'FKP': '\u00a3',
-    'FJD': '\u0024',
-    'GHC': '\u00a2',
-    'GIP': '\u00a3',
-    'GTQ': '\u0051',
-    'GGP': '\u00a3',
-    'GYD': '\u0024',
-    'HNL': '\u004c',
-    'HKD': '\u0024',
-    'HUF': '\u0046\u0074',
-    'ISK': '\u006b\u0072',
-    'INR': '\u20b9',
-    'IDR': '\u0052\u0070',
-    'IRR': '\ufdfc',
-    'IMP': '\u00a3',
-    'ILS': '\u20aa',
-    'JMD': '\u004a\u0024',
-    'JPY': '\u00a5',
-    'JEP': '\u00a3',
-    'KZT': '\u043b\u0432',
-    'KPW': '\u20a9',
-    'KRW': '\u20a9',
-    'KGS': '\u043b\u0432',
-    'LAK': '\u20ad',
-    'LVL': '\u004c\u0073',
-    'LBP': '\u00a3',
-    'LRD': '\u0024',
-    'LTL': '\u004c\u0074',
-    'MKD': '\u0434\u0435\u043d',
-    'MYR': '\u0052\u004d',
-    'MUR': '\u20a8',
-    'MXN': '\u0024',
-    'MNT': '\u20ae',
-    'MZN': '\u004d\u0054',
-    'NAD': '\u0024',
-    'NPR': '\u20a8',
-    'ANG': '\u0192',
-    'NZD': '\u0024',
-    'NIO': '\u0043\u0024',
-    'NGN': '\u20a6',
-    'NOK': '\u006b\u0072',
-    'OMR': '\ufdfc',
-    'PKR': '\u20a8',
-    'PAB': '\u0042\u002f\u002e',
-    'PYG': '\u0047\u0073',
-    'PEN': '\u0053\u002f\u002e',
-    'PHP': '\u20b1',
-    'PLN': '\u007a\u0142',
-    'QAR': '\ufdfc',
-    'RON': '\u006c\u0065\u0069',
-    'RUB': '\u0440\u0443\u0431',
-    'SHP': '\u00a3',
-    'SAR': '\ufdfc',
-    'RSD': '\u0414\u0438\u043d\u002e',
-    'SCR': '\u20a8',
-    'SGD': '\u0024',
-    'SBD': '\u0024',
-    'SOS': '\u0053',
-    'ZAR': '\u0052',
-    'LKR': '\u20a8',
-    'SEK': '\u006b\u0072',
-    'CHF': '\u0043\u0048\u0046',
-    'SRD': '\u0024',
-    'SYP': '\u00a3',
-    'TWD': '\u004e\u0054\u0024',
-    'THB': '\u0e3f',
-    'TTD': '\u0054\u0054\u0024',
-    'TRY': '\u20ba',
-    'TRL': '\u20a4',
-    'TVD': '\u0024',
-    'UAH': '\u20b4',
-    'GBP': '\u00a3',
-    'USD': '\u0024',
-    'UYU': '\u0024\u0055',
-    'UZS': '\u043b\u0432',
-    'VEF': '\u0042\u0073',
-    'VND': '\u20ab',
-    'YER': '\ufdfc',
-    'ZWD': '\u005a\u0024'
-  });
 
 
   /*
@@ -239,8 +78,6 @@
   
   @name Dynamic Locale Provider
   @description
-  
-  @todo Add configurability since it is a provider
    */
 
   dl.provider('dlProvider', [
@@ -314,6 +151,116 @@
       return this;
     }
   ]);
+
+  app = angular.module('app', ['dl']);
+
+  app.config([
+    'dlProviderProvider', function(dlProviderProvider) {
+      dlProviderProvider.setDefaultLocale('en-us');
+      dlProviderProvider.setLocales(['af-na', 'af-za', 'af', 'am-et', 'am', 'ar-001', 'ar-ae', 'ar-bh', 'ar-dz', 'ar-eg', 'ar-iq', 'ar-jo', 'ar-kw', 'ar-lb', 'ar-ly', 'ar-ma', 'ar-om', 'ar-qa', 'ar-sa', 'ar-sd', 'ar-sy', 'ar-tn', 'ar-ye', 'ar', 'bg-bg', 'bg', 'bn-bd', 'bn-in', 'bn', 'ca-ad', 'ca-es', 'ca', 'cs-cz', 'cs', 'da-dk', 'da', 'de-at', 'de-be', 'de-ch', 'de-de', 'de-li', 'de-lu', 'de', 'el-cy', 'el-gr', 'el', 'en-as', 'en-au', 'en-bb', 'en-be', 'en-bm', 'en-bw', 'en-bz', 'en-ca', 'en-dsrt-us', 'en-dsrt', 'en-fm', 'en-gb', 'en-gu', 'en-gy', 'en-hk', 'en-ie', 'en-in', 'en-iso', 'en-jm', 'en-mh', 'en-mp', 'en-mt', 'en-mu', 'en-na', 'en-nz', 'en-ph', 'en-pk', 'en-pr', 'en-pw', 'en-sg', 'en-tc', 'en-tt', 'en-um', 'en-us', 'en-vg', 'en-vi', 'en-za', 'en-zw', 'en', 'es-419', 'es-ar', 'es-bo', 'es-cl', 'es-co', 'es-cr', 'es-do', 'es-ea', 'es-ec', 'es-es', 'es-gq', 'es-gt', 'es-hn', 'es-ic', 'es-mx', 'es-ni', 'es-pa', 'es-pe', 'es-pr', 'es-py', 'es-sv', 'es-us', 'es-uy', 'es-ve', 'es', 'et-ee', 'et', 'eu-es', 'eu', 'fa-af', 'fa-ir', 'fa', 'fi-fi', 'fi', 'fil-ph', 'fil', 'fr-be', 'fr-bf', 'fr-bi', 'fr-bj', 'fr-bl', 'fr-ca', 'fr-cd', 'fr-cf', 'fr-cg', 'fr-ch', 'fr-ci', 'fr-cm', 'fr-dj', 'fr-fr', 'fr-ga', 'fr-gf', 'fr-gn', 'fr-gp', 'fr-gq', 'fr-km', 'fr-lu', 'fr-mc', 'fr-mf', 'fr-mg', 'fr-ml', 'fr-mq', 'fr-ne', 'fr-re', 'fr-yt', 'fr', 'gl-es', 'gl', 'gsw-ch', 'gsw', 'gu-in', 'gu', 'he-il', 'he', 'hi-in', 'hi', 'hr-hr', 'hr', 'hu-hu', 'hu', 'id-id', 'id', 'in', 'is-is', 'is', 'it-it', 'it-sm', 'it', 'iw', 'ja-jp', 'ja', 'kn-in', 'kn', 'ko-kr', 'ko', 'ln-cd', 'ln', 'lt-lt', 'lt', 'lv-lv', 'lv', 'ml-in', 'ml', 'mr-in', 'mr', 'ms-my', 'ms', 'mt-mt', 'mt', 'nl-cw', 'nl-nl', 'nl-sx', 'nl', 'no', 'or-in', 'or', 'pl-pl', 'pl', 'pt-br', 'pt-pt', 'pt', 'ro-ro', 'ro', 'ru-ru', 'ru', 'sk-sk', 'sk', 'sl-si', 'sl', 'sq-al', 'sq', 'sr-cyrl-rs', 'sr-latn-rs', 'sr', 'sv-se', 'sv', 'sw-tz', 'sw', 'ta-in', 'ta', 'te-in', 'te', 'th-th', 'th', 'tl', 'tr-tr', 'tr', 'uk-ua', 'uk', 'ur-pk', 'ur', 'vi-vn', 'vi', 'zh-cn', 'zh-hans-cn', 'zh-hk', 'zh-tw', 'zh', 'zu-za', 'zu']);
+      dlProviderProvider.setCurrencyMap({
+        'CAD': 'en-ca',
+        'EUR': 'de',
+        'GBP': 'en-gb',
+        'USD': 'en-us'
+      });
+      return dlProviderProvider.loadLocales();
+    }
+  ]);
+
+  app.controller('AngularDynamicLocaleController', [
+    '$filter', '$scope', '$window', 'ccCurrencySymbol', 'dlProvider', function($filter, $scope, $window, ccCurrencySymbol, dlProvider) {
+      var filterInternationalResults, filterLocaleResults, international_total_pages, international_values, key, localeMap, locale_total_pages, locale_values, number_of_international_values, number_of_locale_values, per_page, value, x;
+      localeMap = ['af-na', 'af-za', 'af', 'am-et', 'am', 'ar-001', 'ar-ae', 'ar-bh', 'ar-dz', 'ar-eg', 'ar-iq', 'ar-jo', 'ar-kw', 'ar-lb', 'ar-ly', 'ar-ma', 'ar-om', 'ar-qa', 'ar-sa', 'ar-sd', 'ar-sy', 'ar-tn', 'ar-ye', 'ar', 'bg-bg', 'bg', 'bn-bd', 'bn-in', 'bn', 'ca-ad', 'ca-es', 'ca', 'cs-cz', 'cs', 'da-dk', 'da', 'de-at', 'de-be', 'de-ch', 'de-de', 'de-li', 'de-lu', 'de', 'el-cy', 'el-gr', 'el', 'en-as', 'en-au', 'en-bb', 'en-be', 'en-bm', 'en-bw', 'en-bz', 'en-ca', 'en-dsrt-us', 'en-dsrt', 'en-fm', 'en-gb', 'en-gu', 'en-gy', 'en-hk', 'en-ie', 'en-in', 'en-iso', 'en-jm', 'en-mh', 'en-mp', 'en-mt', 'en-mu', 'en-na', 'en-nz', 'en-ph', 'en-pk', 'en-pr', 'en-pw', 'en-sg', 'en-tc', 'en-tt', 'en-um', 'en-us', 'en-vg', 'en-vi', 'en-za', 'en-zw', 'en', 'es-419', 'es-ar', 'es-bo', 'es-cl', 'es-co', 'es-cr', 'es-do', 'es-ea', 'es-ec', 'es-es', 'es-gq', 'es-gt', 'es-hn', 'es-ic', 'es-mx', 'es-ni', 'es-pa', 'es-pe', 'es-pr', 'es-py', 'es-sv', 'es-us', 'es-uy', 'es-ve', 'es', 'et-ee', 'et', 'eu-es', 'eu', 'fa-af', 'fa-ir', 'fa', 'fi-fi', 'fi', 'fil-ph', 'fil', 'fr-be', 'fr-bf', 'fr-bi', 'fr-bj', 'fr-bl', 'fr-ca', 'fr-cd', 'fr-cf', 'fr-cg', 'fr-ch', 'fr-ci', 'fr-cm', 'fr-dj', 'fr-fr', 'fr-ga', 'fr-gf', 'fr-gn', 'fr-gp', 'fr-gq', 'fr-km', 'fr-lu', 'fr-mc', 'fr-mf', 'fr-mg', 'fr-ml', 'fr-mq', 'fr-ne', 'fr-re', 'fr-yt', 'fr', 'gl-es', 'gl', 'gsw-ch', 'gsw', 'gu-in', 'gu', 'he-il', 'he', 'hi-in', 'hi', 'hr-hr', 'hr', 'hu-hu', 'hu', 'id-id', 'id', 'in', 'is-is', 'is', 'it-it', 'it-sm', 'it', 'iw', 'ja-jp', 'ja', 'kn-in', 'kn', 'ko-kr', 'ko', 'ln-cd', 'ln', 'lt-lt', 'lt', 'lv-lv', 'lv', 'ml-in', 'ml', 'mr-in', 'mr', 'ms-my', 'ms', 'mt-mt', 'mt', 'nl-cw', 'nl-nl', 'nl-sx', 'nl', 'no', 'or-in', 'or', 'pl-pl', 'pl', 'pt-br', 'pt-pt', 'pt', 'ro-ro', 'ro', 'ru-ru', 'ru', 'sk-sk', 'sk', 'sl-si', 'sl', 'sq-al', 'sq', 'sr-cyrl-rs', 'sr-latn-rs', 'sr', 'sv-se', 'sv', 'sw-tz', 'sw', 'ta-in', 'ta', 'te-in', 'te', 'th-th', 'th', 'tl', 'tr-tr', 'tr', 'uk-ua', 'uk', 'ur-pk', 'ur', 'vi-vn', 'vi', 'zh-cn', 'zh-hans-cn', 'zh-hk', 'zh-tw', 'zh', 'zu-za', 'zu'];
+      locale_values = (function() {
+        var _i, _ref, _results;
+        _results = [];
+        for (x = _i = 1, _ref = localeMap.length - 1; 1 <= _ref ? _i <= _ref : _i >= _ref; x = 1 <= _ref ? ++_i : --_i) {
+          _results.push({
+            amount: parseFloat(Math.random() * 5000).toFixed(2),
+            locale: localeMap[x]
+          });
+        }
+        return _results;
+      })();
+      international_values = (function() {
+        var _results;
+        _results = [];
+        for (key in ccCurrencySymbol) {
+          value = ccCurrencySymbol[key];
+          _results.push({
+            amount: parseFloat(Math.random() * 5000).toFixed(2),
+            currency_code: key
+          });
+        }
+        return _results;
+      })();
+      per_page = 15;
+      number_of_locale_values = locale_values.length;
+      locale_total_pages = Math.ceil(number_of_locale_values / per_page);
+      $scope.locale_page = 1;
+      filterLocaleResults = function() {
+        var offset, results;
+        offset = $scope.locale_page * per_page - per_page;
+        results = $filter("filter")(locale_values, $scope.searchLocale);
+        locale_total_pages = Math.ceil(results.length / per_page);
+        return $scope.locale_values = results.slice(offset, offset + per_page);
+      };
+      number_of_international_values = international_values.length;
+      international_total_pages = Math.ceil(number_of_international_values / per_page);
+      $scope.international_page = 1;
+      filterInternationalResults = function() {
+        var offset, results;
+        offset = $scope.international_page * per_page - per_page;
+        results = $filter("filter")(international_values, $scope.searchInternational);
+        international_total_pages = Math.ceil(results.length / per_page);
+        return $scope.international_values = results.slice(offset, offset + per_page);
+      };
+      $scope.cc_currency_symbol = ccCurrencySymbol;
+      $scope.date = new Date();
+      $scope.goHome = function() {
+        if ($window.location.hostname === 'localhost') {
+          return '/';
+        } else {
+          return '/angular-dynamic-locale';
+        }
+      };
+      $scope.paginateFirst = function(type) {
+        return $scope["" + type + "_page"] = 1;
+      };
+      $scope.paginateLast = function(type) {
+        return $scope["" + type + "_page"] = type === 'international' ? international_total_pages : locale_total_pages;
+      };
+      $scope.paginateNext = function(type) {
+        return $scope["" + type + "_page"] = type === 'international' ? $scope.international_page === international_total_pages ? international_total_pages : $scope.international_page + 1 : $scope.locale_page === locale_total_pages ? locale_total_pages : $scope.locale_page + 1;
+      };
+      $scope.paginatePrevious = function(type) {
+        return $scope["" + type + "_page"] = type === 'international' ? $scope.international_page === 1 ? 1 : $scope.international_page - 1 : $scope.locale_page === 1 ? 1 : $scope.locale_page - 1;
+      };
+      $scope.$watch('searchLocale.locale', function(newValue, oldValue) {
+        if (newValue === oldValue) {
+          return;
+        }
+        $scope.locale_page = 1;
+        return filterLocaleResults();
+      });
+      $scope.$watch('locale_page', function(newValue, oldValue) {
+        return filterLocaleResults();
+      });
+      $scope.$watch('searchInternational.currency_code', function(newValue, oldValue) {
+        if (newValue === oldValue) {
+          return;
+        }
+        $scope.international_page = 1;
+        return filterInternationalResults();
+      });
+      return $scope.$watch('international_page', function(newValue, oldValue) {
+        return filterInternationalResults();
+      });
+    }
+  ]);
+
+  $(document).foundation();
 
   dl.constant("dlConstant", {
     "af-na": "'use strict';\nangular.module(\"ngLocale\", [], [\"$provide\", function($provide) {\nvar PLURAL_CATEGORY = {ZERO: \"zero\", ONE: \"one\", TWO: \"two\", FEW: \"few\", MANY: \"many\", OTHER: \"other\"};\n$provide.value(\"$locale\", {\n  \"DATETIME_FORMATS\": {\n    \"AMPMS\": [\n      \"vm.\",\n      \"nm.\"\n    ],\n    \"DAY\": [\n      \"Sondag\",\n      \"Maandag\",\n      \"Dinsdag\",\n      \"Woensdag\",\n      \"Donderdag\",\n      \"Vrydag\",\n      \"Saterdag\"\n    ],\n    \"MONTH\": [\n      \"Januarie\",\n      \"Februarie\",\n      \"Maart\",\n      \"April\",\n      \"Mei\",\n      \"Junie\",\n      \"Julie\",\n      \"Augustus\",\n      \"September\",\n      \"Oktober\",\n      \"November\",\n      \"Desember\"\n    ],\n    \"SHORTDAY\": [\n      \"So\",\n      \"Ma\",\n      \"Di\",\n      \"Wo\",\n      \"Do\",\n      \"Vr\",\n      \"Sa\"\n    ],\n    \"SHORTMONTH\": [\n      \"Jan\",\n      \"Feb\",\n      \"Mar\",\n      \"Apr\",\n      \"Mei\",\n      \"Jun\",\n      \"Jul\",\n      \"Aug\",\n      \"Sep\",\n      \"Okt\",\n      \"Nov\",\n      \"Des\"\n    ],\n    \"fullDate\": \"EEEE d MMMM y\",\n    \"longDate\": \"d MMMM y\",\n    \"medium\": \"d MMM y HH:mm:ss\",\n    \"mediumDate\": \"d MMM y\",\n    \"mediumTime\": \"HH:mm:ss\",\n    \"short\": \"yyyy-MM-dd HH:mm\",\n    \"shortDate\": \"yyyy-MM-dd\",\n    \"shortTime\": \"HH:mm\"\n  },\n  \"NUMBER_FORMATS\": {\n    \"CURRENCY_SYM\": \"R\",\n    \"DECIMAL_SEP\": \",\",\n    \"GROUP_SEP\": \"\u00a0\",\n    \"PATTERNS\": [\n      {\n        \"gSize\": 3,\n        \"lgSize\": 3,\n        \"macFrac\": 0,\n        \"maxFrac\": 3,\n        \"minFrac\": 0,\n        \"minInt\": 1,\n        \"negPre\": \"-\",\n        \"negSuf\": \"\",\n        \"posPre\": \"\",\n        \"posSuf\": \"\"\n      },\n      {\n        \"gSize\": 3,\n        \"lgSize\": 3,\n        \"macFrac\": 0,\n        \"maxFrac\": 2,\n        \"minFrac\": 2,\n        \"minInt\": 1,\n        \"negPre\": \"(\u00a4\",\n        \"negSuf\": \")\",\n        \"posPre\": \"\u00a4\",\n        \"posSuf\": \"\"\n      }\n    ]\n  },\n  \"id\": \"af-na\",\n  \"pluralCat\": function (n) {  if (n == 1) {   return PLURAL_CATEGORY.ONE;  }  return PLURAL_CATEGORY.OTHER;}\n});\n}]);",
